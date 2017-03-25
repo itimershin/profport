2643815234c33edc035c4ec6970f1e46438fce86
Andrey <im@andreystarkov>
2017-03-25 14:07:52 +0500

    Devise using, validates to passwd 1,2 step

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  validates :email, presence: true
+  validates :password, length: 6..20
+end
