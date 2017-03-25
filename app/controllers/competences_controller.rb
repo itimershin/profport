e47d942e77b57bc0f9e660b71ff49aed84588db4
Andrey <im@andreystarkov>
2017-03-25 15:26:40 +0500

    step 4 building

---
+++ app/controllers/competences_controller.rb
@@ -0,0 +1,53 @@
+class CompetenceController < ApplicationController
+  before_action :set_competence, only: [:show, :update, :edit, :destroy]
+  def index
+    @competences = Competence.all
+  end
+  def show
+  end
+
+  def new
+    @competence = Competence.new
+  end
+
+  def create
+    @competence = Competence.new
+    respond_to do |f|
+      if @competence.save (competence_params)
+        f.html {redirect_to @competence}
+      else
+        f.html {render :new}
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |f|
+      if @competence.update (competence_params)
+        f.html {redirect_to @competence}
+      else
+        f.html {render :edit}
+
+      end
+    end
+  end
+
+  def destroy
+    if @competence.destroy
+      respond_to do |f|
+        f.html {redirect_to competences_url}
+      end
+    end
+  end
+
+  private
+  def set_competence
+    @competence = Competence.find (params[:id])
+  end
+  def competence_params
+    params.require(:competence).permit(:title, :author, :skillrate)
+  end
+end
--- db/migrate/20170325091024_create_competences.rb
+++ db/migrate/20170325091024_create_competences.rb
@@ -1,8 +1,11 @@
 class CreateCompetences < ActiveRecord::Migration[5.0]
   def change
     create_table :competences do |t|
-
+      t.string :title
+      t.string :author
+      t.integer :skillrate
       t.timestamps
     end
+    add_index :competences, :title, unique: true
   end
 end
